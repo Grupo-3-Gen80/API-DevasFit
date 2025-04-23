609097df86b57638b10bf802f4302bead0ccb77d
DiasRay <diassilvarayanne@gmail.com>
2025-04-23 09:07:00 -0300

    Delete Usuario

--- src/main/java/com/generation/devasfit/controller/UsuarioController.java
+++ src/main/java/com/generation/devasfit/controller/UsuarioController.java
@@ -1,19 +1,24 @@
 package com.generation.devasfit.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
+import com.generation.devasfit.model.Categoria;
 import com.generation.devasfit.model.Usuario;
 import com.generation.devasfit.repository.UsuarioRepository;
 import com.generation.devasfit.service.UsuarioService;
@@ -68,5 +73,16 @@ public class UsuarioController {
 	public ResponseEntity<String> getIMC(@PathVariable Long id) {
 	    return ResponseEntity.ok(usuarioService.calcularIMC(id));
 	}
+	
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id) {
+        Optional<Usuario> usuario = usuarioRepository.findById(id);
+        
+        if(usuario.isEmpty())
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        
+        usuarioRepository.deleteById(id);              
+    }
 
 }
